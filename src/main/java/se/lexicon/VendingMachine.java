243da07635a98506dd45b6f4ed566dcdb306441a
John Baptist Rukarugahieguru <rukaruga@yahoo.co.uk>
2024-07-04 19:27:53 +0200

    Created

---
+++ src/main/java/se/lexicon/VendingMachine.java
@@ -0,0 +1,15 @@
+package se.lexicon;
+
+public interface VendingMachine {
+    void addCurrency(int amount);
+
+    int getBalance();
+
+    Product request(int id);
+
+    int endSession();
+
+    String getDescription(int id);
+
+    String[] getProducts();
+}
