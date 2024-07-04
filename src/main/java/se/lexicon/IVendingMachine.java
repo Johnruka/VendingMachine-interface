1d9fb5b4d52e69d33b4a03084702a66319f47902
John Baptist Rukarugahieguru <rukaruga@yahoo.co.uk>
2024-07-04 19:41:12 +0200

    Modified

---
+++ src/main/java/se/lexicon/IVendingMachine.java
@@ -0,0 +1,79 @@
+package se.lexicon;
+
+public class IVendingMachine implements VendingMachine {
+
+    public static final int[] VALID_DENOMINATIONS = {1, 2, 5, 10, 20, 50, 100, 200, 500, 1000};
+    private final Product[] products;
+    public int depositPool;
+
+    public IVendingMachine(Product[] products) {
+        this.products = products;
+}
+    @Override
+    public void addCurrency(int amount) {
+        if (isValidDenomination(amount)) {
+            depositPool += amount;
+        } else {
+            throw new IllegalArgumentException("Invalid denomination: " + amount);
+
+
+        }
+    }
+
+    private boolean isValidDenomination(int amount) {
+        for (int denomination : VALID_DENOMINATIONS) {
+            if (denomination == amount) {
+                return true;
+            }
+        }
+        return false;
+
+
+    }
+
+    @Override
+    public int getBalance() {
+        return depositPool;
+    }
+
+    @Override
+    public Product request(int id) {
+        Product requestedProduct = findProductById(id);
+        if (requestedProduct != null) {
+            if (requestedProduct.getPrice() <= depositPool) {
+                depositPool -= requestedProduct.getPrice();
+                return requestedProduct;
+            } else {
+                throw new IllegalArgumentException("Insufficient funds for product: " + requestedProduct.getProductName());
+            }
+        } else {
+            throw new IllegalArgumentException("Product not found with id: " + id);
+        }
+    }
+
+    private Product findProductById(int id) {
+        for (Product product : products) {
+            if (product.getId() == id) {
+                return product;
+            }
+        }
+        return null;
+    }
+
+    @Override
+    public int endSession() {
+        int balance = depositPool;
+        depositPool = 0;
+        return balance;
+    }
+
+    @Override
+    public String getDescription(int id) {
+        return "";
+    }
+
+    @Override
+    public String[] getProducts() {
+        return new String[0];
+    }
+}
