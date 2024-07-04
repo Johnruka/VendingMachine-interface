246e5a9f864540e2ee90e8aea8e28cae7023f18d
John Baptist Rukarugahieguru <rukaruga@yahoo.co.uk>
2024-07-04 18:47:36 +0200

    Created

---
+++ src/main/java/se/lexicon/Product.java
@@ -0,0 +1,17 @@
+package se.lexicon;
+
+public interface Product {
+    String examine();
+
+    String use();
+
+    int getId();
+
+    String getProductName();
+
+    void setProductName(String productName);
+
+    double getPrice();
+
+    void setPrice(double price);
+}
