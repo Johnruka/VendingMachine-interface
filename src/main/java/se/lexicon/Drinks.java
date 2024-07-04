ca38035927b5fa63f2684dfa2c09e3d59f6ca941
John Baptist Rukarugahieguru <rukaruga@yahoo.co.uk>
2024-07-04 19:03:19 +0200

    Created

---
+++ src/main/java/se/lexicon/Drinks.java
@@ -0,0 +1,48 @@
+package se.lexicon;
+
+public class Drinks implements Product {
+
+    private final int id;
+    private String drinksName;
+    private String volume;
+    private double price;
+
+    public Drinks(String drinksName,String volume, double price) {
+        this.id = productSequencer.nextId();
+
+   }
+    @Override
+    public String examine() {
+        return "";
+    }
+
+    @Override
+    public String use() {
+        return "";
+    }
+
+    @Override
+    public int getId() {
+        return 0;
+    }
+
+    @Override
+    public String getProductName() {
+        return "";
+    }
+
+    @Override
+    public void setProductName(String productName) {
+
+    }
+
+    @Override
+    public double getPrice() {
+        return 0;
+    }
+
+    @Override
+    public void setPrice(double price) {
+
+    }
+}
